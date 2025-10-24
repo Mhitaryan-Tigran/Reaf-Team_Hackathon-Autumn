2fc2975cffe958f6019737ba4a0923027d214885
nikitatatata <tt.nikita.s@gmail.com>
2025-10-24 22:24:18 +0300

    Доделал базу + gitignore + uv + start.sh

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
-
-class Item(BaseModel):
-    id: int
-    name: str
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"id": item_id, "name": f"item-{item_id}"}
-
-@app.post("/items", status_code=201)
-async def create_item(item: Item):
-    return item
