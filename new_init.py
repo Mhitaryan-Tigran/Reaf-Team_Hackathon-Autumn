319799244a08c758afbc0079dfe6aea06dc93fcb
nikitatatata <tt.nikita.s@gmail.com>
2025-10-25 21:47:31 +0300

    init changes

--- new_init.py
+++ new_init.py
@@ -1,4 +1,10 @@
 import uuid
+import socket
+
+agentUUID = str(uuid.uuid4())
 
 with open(".env", "w") as f:
-    f.write(f"agentUUID={str(uuid.uuid4())}")
+    f.write(f"agentUUID={agentUUID}")
+
+print(f"IP: {socket.gethostbyname(socket.gethostname())}")
+print(agentUUID)
