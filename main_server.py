4f162b76f12f264636742f1f6692bd2f9e73f5eb
nik <tt.nikita.s@gmail.com>
2025-10-25 10:33:37 +0300

    Требования И чут чуть сервера + Заметку по бд в докере

--- main_server.py
+++ main_server.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import psycopg2
+
+# conn = ""
+# cursor = conn.cursor()
+# cursor.execute("SELECT * FROM my_table")
+# rows = cursor.fetchall()
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+@app.on_event("startup")
+async def startDBConnection():
+    global conn
+    conn = psycopg2.connect(database="serverDB", user="SERVER", password="hakatonski123", host="localhost", port="5432")
+
+
+@app.on_event("shutdown")
+async def stopDBConnection():
+    conn.close()
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -24,6 +40,7 @@ def ResultPage(request: Request):
 def AgentStatus(request: Request):
     return templates.TemplateResponse("agent_status.html", {"request": request})
 
+# Api
 @app.post("/takeStatus")
-def getOtschetFromAgent():
+def getReportFromAgent():
     pass
