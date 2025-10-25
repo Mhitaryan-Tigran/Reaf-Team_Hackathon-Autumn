0151681cbb68e58167ed30b4eaa4d14d162d9706
nik <tt.nikita.s@gmail.com>
2025-10-25 14:18:20 +0300

    preFinal_main_server

--- main_server.py
+++ main_server.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import psycopg2
 from pydantic import BaseModel
+import uuid
 
 # conn = ""
 # cursor = conn.cursor()
@@ -43,16 +44,45 @@ def AgentStatus(request: Request):
 
 # Api
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # логика: эхо обратно
+            
+            cursor = conn.cursor()
+            cursor.execute("select * from reports where uiid=%s", (data,))
+            rows = cursor.fetchall()
+            cursor.close()
+
+            await websocket.send_text(rows)
+    except WebSocketDisconnect:
+        # клиент отключился
+        pass
+
 class reportFromAgent(BaseModel):
     country: str
     UIID: str
     taskUIID: str
-    task: str
     result: str
 
 class checkRequest(BaseModel):
     target: str
     task: str
+
+@app.post("/getReports")
+async def getReports(request: Request):
+    rawBody = await request.body()
+    clearBody = rawBody.decode("utf-8")
+    print(clearBody)
+    cursor = conn.cursor()
+    cursor.execute("select * from reports where uiid=%s", (clearBody,))
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.close()
+    return rows
 
 @app.post("/takeReport")
 def getReportFromAgent(report: reportFromAgent):
@@ -62,7 +92,7 @@ def getReportFromAgent(report: reportFromAgent):
     cursor.close()
     if rows[0][0]:
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO Reports (country, UIID, task, result) VALUES (%s, %s, %s, %s);", (report.country, report.taskUIID, report.task, report.result))
+        cursor.execute("INSERT INTO Reports (country, UIID, result) VALUES (%s, %s, %s);", (report.country, report.taskUIID, report.result))
         conn.commit()
         cursor.close()
         return Response(status_code=200)
@@ -71,10 +101,20 @@ def getReportFromAgent(report: reportFromAgent):
 
 @app.post("/start_check")
 def startCheck(req: checkRequest):
+    taskUIID = uuid.uuid4()
     cursor = conn.cursor()
     cursor.execute("SELECT ip FROM Agents")
     rows = cursor.fetchall()
     ipAddrs = []
     for row in rows:
         ipAddrs.append(row[0])
-    print(ipAddrs)
+    print(ipAddrs, taskUIID)
+
+    cursor.execute("INSERT INTO Tasks (UIID, target, task) VALUES (%s, %s, %s);", (str(taskUIID), req.target, req.task))
+    conn.commit()
+
+    match req.task:
+        case "ping":
+            for ip in ipAddrs:
+                print("ping", ip)
+                print(taskUIID, req)
